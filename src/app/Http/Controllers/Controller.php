d606df9513ae9b0d77296df3ac6b81b4665d72cf
bgreatfit <mojemoron2009@yahoo.com>
2023-09-14 17:38:31 +0100

    add swagger documentation to user create endpoint

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -6,7 +6,32 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="Laravel Swagger API documentation example",
+ *     version="1.0.0",
+ *     @OA\Contact(
+ *         email="mioshine2011@gmail.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * @OA\Server(
+ *     description="Laravel Swagger API server",
+ *     url="http://localhost:8080/api/v1/"
+ * )
+ * @OA\SecurityScheme(
+ *     type="apiKey",
+ *     in="header",
+ *     name="X-APP-ID",
+ *     securityScheme="X-APP-ID"
+ * )
+ */
+
 class Controller extends BaseController
 {
+
     use AuthorizesRequests, ValidatesRequests;
 }
--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -8,9 +8,50 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
+
 class UserController extends Controller
 {
-    //
+    /**
+     * @OA\Post(
+     *     path="/user/create",
+     *     tags={"User"},
+     *     summary="Create a User account",
+     *     description="User API endpoint",
+     *     operationId="user-create",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="User data",
+     *        @OA\JsonContent(),
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                type="object",
+     *                required={"first_name", "last_name", "email", "password", "password_confirmation", "address", "phone_number"},
+     *                 @OA\Property(property="first_name", type="string", description="User firstname"),
+     *                 @OA\Property(property="last_name", type="string", description="User lastname"),
+     *                 @OA\Property(property="email", type="string", description="User email"),
+     *                 @OA\Property(property="password", type="string", description="User password"),
+     *                 @OA\Property(property="password_confirmation", type="string", description="User password"),
+     *                 @OA\Property(property="avatar", type="string", description="Avatar image UUID"),
+     *                 @OA\Property(property="address", type="string", description="User main address"),
+     *                 @OA\Property(property="phone_number", type="string", description="User main phone number"),
+     *                 @OA\Property(property="is_marketing", type="string", description="User marketing preferences")
+     *             )
+     *         )
+     *     ),
+     *
+     *    @OA\Response(response=200, description="OK"),
+     *     @OA\Response(response=401, description="Unauthorized"),
+     *     @OA\Response(response=404, description="Page not found"),
+     *     @OA\Response(response=422, description="Unprocessable Entity"),
+     *     @OA\Response(response=500, description="Internal server error"),
+     *     security={{"bearerAuth": {}}}
+     * )
+     * @param \App\Http\Requests\CreateUserRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+
     public function store(CreateUserRequest $request): JsonResponse
     {
         $user = User::create([
